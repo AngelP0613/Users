a5490c82d50be07e6c5446c770f54b0d4a0c2f59
Angelp0796 <andromed32@gmail.com>
2022-10-28 19:42:39 -0500

    condicion email

--- src/routes/user.js
+++ src/routes/user.js
@@ -9,7 +9,21 @@ const User =require("../models/user.model")
 //crear usuario
 router.post("/create-user",async(req, res)=>{
     
+    let {email} = req.body
     
+    
+    //let email = req.body.email
+    email = email.toLowercase()
+    req.body.email = email
+    //obtener usuarios
+    let userExist = await User.find({email: email})
+    
+
+    //validar si existe el emaol
+    if(userExist.length !== 0){
+        return res.sendStatus(400).json({msg: "usuario ya existe"})
+    }
+    //guardar o mandar error
     const newUser = new User(req.body)
     await newUser.save()
     res.status(201).json({msg:"usuario registrado"})
